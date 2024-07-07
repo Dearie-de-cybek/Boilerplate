d1d902a48ee6502eac11bf2e2c078621cc1beccf
Dearie <eburujacintha@gmail.com>
2024-07-07 01:05:46 +0100

    updated the admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,53 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function getAllUsers(Request $request)
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function adminLogin(Request $request)
+{
+    $credentials = $request->only(['email', 'password']);
+    if (auth()->attempt($credentials)) {
+        // login successful, return a token or a success response
+        $user = auth()->user();
+        $token = $user->createToken('admin_token')->plainTextToken;
+        return response()->json(['token' => $token]);
+    } else {
+        // login failed, return an error response
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+
+}
+
+public function updateUser(Request $request, $userId)
+{
+    $user = User::find($userId);
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        // Define validation rules
+        $rules = [
+            'btc_balance' => 'nullable|numeric',
+            'eth_balance' => 'nullable|numeric',
+            'btc_unit' => 'nullable|numeric',
+            'eth_unit' => 'nullable|numeric',
+        ];
+
+        // Validate request data
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Update user with only validated and non-null data
+        $validatedData = $validator->validated();
+        $user->update($validatedData);
+
+        return response()->json(['message' => 'User updated successfully']);
+    }
 }
